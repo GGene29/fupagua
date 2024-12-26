73dc50b8ae6e5463b3e177d655bc34c851b6b0e6
GGene29 <sumozacalcurian@gmail.com>
2024-12-26 19:32:04 -0400

    [FIX]-Etapa IV y V

--- fupagua_system/models/initial_questionnarie.py
+++ fupagua_system/models/initial_questionnarie.py
@@ -5,9 +5,11 @@ class Questionnarie(models.Model):
     _description = 'Cuestionario Inicial es lo mazimo para poder compartir '
     _rec_name = 'names'
     
-    registration_code = fields.Char(string="Code")
+    registration_code = fields.Char(string="Code History")
     
     registration_date = fields.Date(string='Date')
+    
+    # Datos Personales
     
     names = fields.Char(string='Names')
     
@@ -25,9 +27,12 @@ class Questionnarie(models.Model):
     
     age = fields.Integer(string='Age')
     
-    # sex = fields.
+    sex = fields.Selection( [('1', 'F'), ('2', 'M')], string="Sexo")
     
-    # schooling = fields.Select()
+    schooling = fields.Selection([
+        ('0', 'Inicial'),
+        ('1', 'Primaria'),
+        ('2', 'Media')], string="Schooling")
     
     institution = fields.Char(string='Institution')
     
@@ -39,7 +44,7 @@ class Questionnarie(models.Model):
     
     relationship = fields.Char(string='Kinship relationship with the evaluated')
     
-    # Parte II
+    # Parte II Motivo Consulta
     
     evaluation_reason = fields.Text(string="evaluation_reason")
     
@@ -51,7 +56,7 @@ class Questionnarie(models.Model):
     
     treatment_evolution = fields.Text(string="Treatment and evoluction")
     
-    # Parte III
+    # Parte III Familiar
     
     mother_name = fields.Char(string="Mother Name")
     
@@ -108,4 +113,104 @@ class Questionnarie(models.Model):
     father_relationship = fields.Char(string="Relationship")
     
     opinion_behavior_father = fields.Text(string="opinion_behavior_father")
+    
+    family_dynamics = fields.Text(string="Family Dynamics")
+    
+    family_relationship = fields.Text(string="Family relationship")
+    
+    caragiver = fields.Selection([
+        ("1" , "Mother"),
+        ("2", "Father"),
+        ("3", "Other")], 
+        string="Caraviger"),
+    
+    name_caragiver = fields.Char(string="Full name caragiver")
+    
+    caragiver_age = fields.Integer(string="Caragiver age")
+    
+    caragiver_nationality = fields.Selection([
+        ('1', 'Venezolano'),
+        ('2', 'Extranjero')], string="Nationality", default='1')
+    
+    caragiver_id = fields.Integer(string="Caragiver ID")
+    
+    caragiver_educational = fields.Char(string="Educational")
+    
+    caragiver_occupation = fields.Char(string="Occupation Caragiver")
+    
+    caragiver_work_schedule = fields.Char(string="Work Schedule")
+    
+    caragiver_salary = fields.Integer(string="Salary")
+    
+    caragiver_support = fields.Text(string="Support Family")
+    
+    caragiver_status = fields.Char(string="Marital Status")
+    
+    caragiver_phone = fields.Integer(string="Phone")
+    
+    caragiver_relationship = fields.Char(string="Relationship")
+    
+    opinion_behavior_caragiver = fields.Text(string="opinion_behavior_father")
+    
+    # General Familia
+    
+    another_need_help = fields.Text(string="Another needs help")
+    
+    bioligical_condition = fields.Text(string="Bioligical parents condition")
+    
+    family_condition = fields.Text(string="Family Condition")
+    
+    adopted_child = fields.Boolean(string="Adopted")
+    
+    adoption_age = fields.Integer(string="Adoption process age")
+    
+    adoption_agreement = fields.Boolean(string="adoption agreement")
+    
+    knows_adopted = fields.Boolean(string="Knows he is adopted")
+    
+    reaction_adopted_family = fields.Char(string="Family reaction to adoption")
+    
+    # IV Datos Socioeconómicos
+    
+    type_housing = fields.Selection([
+        ('0', 'house'),
+        ('1','apartment'),
+        ('2','farm'),
+        ('3','ranch')], string="Type of Housing")
+    
+    describe_coexistence = fields.Text('Describe coexistence')
+    
+    # V Antecedentes
+    
+    planned_pregnancy = fields.Boolean(string="Planned Pregnancy")
+    
+    desired_child = fields.Boolean(string="Desired child")
+        
+    was_desired = fields.Text(string="Was desired")
+    
+    abortion_attempt = fields.Text(string="Abortion attempt")
+    
+    desired_child_sex = fields.Text(string="Sex of the child was desired")
+    
+    pregnancy_attitude = fields.Text(string="Pregnancy attirude parent")
+    
+    number_pregnancy = fields.Integer(string="Number of pregnancies")
+    
+    previous_abortion = fields.Boolean(string="Previous Abortion")
+    
+    type_abortion = fields.Selection(
+        [('0', 'induce'),
+        ('1', 'natural')], string="type abortion")
+    
+    # pregnancy_type_complication = fields.Many2many('complication_pregnancy')
+    
+    other_complication = fields.Text(string="Other")
+    
+    # habits_pregnancy = fields.Many2many('habits_pregnancy')
+    
+    habits_others = fields.Text(string="Habits Others")
+    
+    # VI Nacimiento 
+    
+    
     
